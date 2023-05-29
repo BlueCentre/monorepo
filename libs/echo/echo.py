33d0ed5a13bebe81ae4295caaff95a5fbfba720d
James Nguyen <james.nguyen@flyrlabs.com>
2023-05-29 22:32:17 +0000

    Refactor app name

--- libs/echo/echo.py
+++ libs/echo/echo.py
@@ -1,7 +1,1 @@
-class Echo:
-    
-    def __init__(self, message=None):
-        self.message = message
-
-    def __str__(self):
-        return self.message or "No message was passed"
+src/echo.py
