716f2065473f389a440881bdd617f80b2cc42103
James Nguyen <james.nguyen@flyrlabs.com>
2023-06-04 01:16:02 +0000

    Working libs tests now!

--- libs/calculator/calculator_test.py
+++ libs/calculator/calculator_test.py
@@ -12,5 +12,9 @@ class TestSum(unittest.TestCase):
     calculator = Calculator()
     self.assertEqual(calculator.add(-4, -11), -15)
 
+  def test_sum_reality(self):
+    calculator = Calculator()
+    self.assertNotEqual(calculator.add(1, 2), 0)
+
 if __name__ == '__main__':
   unittest.main()
