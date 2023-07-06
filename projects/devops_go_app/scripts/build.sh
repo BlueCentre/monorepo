4b8f38fb09df3b5f095ea8abf7e75efc544d40d6
James H. Nguyen <james.nguyen@flyrlabs.com>
2023-07-05 22:44:13 -0700

    refactor: devops_go_app (#33)

--- projects/devops_go_app/scripts/build.sh
+++ projects/devops_go_app/scripts/build.sh
@@ -12,13 +12,14 @@ echo "PLATFORMS: ${PLATFORMS}"
 echo "SKIP_TEST: ${SKIP_TEST}"
 echo "===[DEBUG] build.sh==="
 
-bazel build //projects/go_devops_cli_app:tarball
+# If we can't build, fail quick and exit
+bazel build //projects/devops_go_app:tarball || exit 1
 
-# TAR_FILE=$(bazel cquery --output=files //projects/go_devops_cli_app:tarball)
+# TAR_FILE=$(bazel cquery --output=files //projects/devops_go_app:tarball)
 # docker load --input ${TAR_FILE}
-TAR_PATH="$(bazel info bazel-bin)/projects/go_devops_cli_app/tarball"
+TAR_PATH="$(bazel info bazel-bin)/projects/devops_go_app/tarball"
 docker load -i ${TAR_PATH}/tarball.tar
-docker tag flyr.io/bazel/go-devops-cli-app ${IMAGE}
+docker tag flyr.io/bazel/devops-go-app ${IMAGE}
 
 if ${PUSH_IMAGE}
 then
