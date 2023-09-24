f383dded01224c647da0ec2a0d8a7d56e2af035c
James Nguyen <james.nguyen@flyrlabs.com>
2023-09-24 16:42:35 +0000

    refactor:PR-7 Changing docker tag

--- projects/devops_go_app/scripts/build.sh
+++ projects/devops_go_app/scripts/build.sh
@@ -19,7 +19,7 @@ bazel build //projects/devops_go_app:tarball || exit 1
 # docker load --input ${TAR_FILE}
 TAR_PATH="$(bazel info bazel-bin)/projects/devops_go_app/tarball"
 docker load -i ${TAR_PATH}/tarball.tar
-docker tag flyr.io/bazel/devops-go-app ${IMAGE}
+docker tag bazel/devops-go-app ${IMAGE}
 
 if ${PUSH_IMAGE}
 then
