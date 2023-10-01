f43834d19977334ac962501cb347a4de6a67db70
James Nguyen <james.nguyen@flyrlabs.com>
2023-10-01 01:05:11 +0000

    update:PR-2 Update rules and folder structure

--- projects/_deprecated/my_other_app/app.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def index():
-    return {"message": "Hello, world!"}
--- projects/_deprecated/py_helloworld_cli_app/app/__main__.py
+++
@@ -1,5 +0,0 @@
-from projects.py_helloworld_cli_app.app.cli import Helloworld
-
-if __name__ == "__main__":
-    app = Helloworld("John")
-    app.say_hello()
