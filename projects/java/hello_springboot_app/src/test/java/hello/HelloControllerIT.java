63e6591d92a491fd5c3456c9092b8cf19d128c13
James Nguyen <james.nguyen@flyrlabs.com>
2023-10-04 06:34:04 +0000

    fix:PR-3 Final fix from recent upgrade

--- projects/java/hello_springboot_app/src/test/java/hello/HelloControllerIT.java
+++ projects/java/hello_springboot_app/src/test/java/hello/HelloControllerIT.java
@@ -5,18 +5,12 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// import java.net.URL;
-
-// import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.test.web.client.TestRestTemplate;
-// import org.springframework.boot.test.web.server.LocalServerPort;
-// import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -36,26 +30,3 @@ public class HelloControllerIT {
         .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
   }
 }
-
-// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// public class HelloControllerIT {
-
-//   @LocalServerPort
-//   private int port;
-
-//   private URL base;
-
-//   @Autowired
-//   private TestRestTemplate template;
-
-//   @Before
-//   public void setUp() throws Exception {
-//     this.base = new URL("http://localhost:" + port + "/");
-//   }
-
-//   @Test
-//   public void getHello() throws Exception {
-//     ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
-//     assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
-//   }
-// }
