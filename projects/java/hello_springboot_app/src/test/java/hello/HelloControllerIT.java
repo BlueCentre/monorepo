ea0af59f66d1f42a708c2726e19ce5653ded465a
James Nguyen <james.nguyen@flyrlabs.com>
2023-10-04 05:46:15 +0000

    fix:PR-2 Fixed hello_springboot_app tests after recent upgrade

--- projects/java/hello_springboot_app/src/test/java/hello/HelloControllerIT.java
+++ projects/java/hello_springboot_app/src/test/java/hello/HelloControllerIT.java
@@ -1,27 +1,40 @@
 package hello;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.equalTo;
 
-import java.net.URL;
-import org.junit.Before;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// import java.net.URL;
+
+// import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.boot.test.web.client.TestRestTemplate;
+// import org.springframework.boot.test.web.server.LocalServerPort;
+// import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class HelloControllerIT {
 
-    @Test
-    public void contextLoads() {
-    }
+  @Autowired private MockMvc mvc;
 
+  @Test
+  public void getHello() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+  }
 }
 
 // @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -45,19 +58,4 @@ public class HelloControllerIT {
 //     ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
 //     assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
 //   }
-// }
-
-// @RunWith(SpringRunner.class)
-// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// public class HelloControllerIT {
-
-//     @Test
-//     void exampleTest(@Autowired WebTestClient webClient) {
-//         webClient
-//             .get().uri("/")
-//             .exchange()
-//             .expectStatus().isOk()
-//             .expectBody(String.class).isEqualTo("Greetings from Spring Boot!");
-//     }
-
 // }
